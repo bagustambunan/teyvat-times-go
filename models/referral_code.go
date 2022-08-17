f2ffb05b87a16ce60c0a1db7ece82ca9a81e7f8b
Muhammad Bagus Syahputra Tambunan <bagus.tambunan@shopee.com>
2022-08-18 00:34:16 +0700

    refactor: delete model ReferralCode

--- models/referral_code.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "gorm.io/gorm"
-
-type ReferralCode struct {
-	gorm.Model `json:"-"`
-	ID         int    `json:"referralCodeID" gorm:"primaryKey"`
-	Code       string `json:"code"`
-}
