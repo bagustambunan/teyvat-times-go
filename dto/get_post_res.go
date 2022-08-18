6843999cb07618e00e8a0e0faf474bc20e15fd2b
Muhammad Bagus Syahputra Tambunan <bagus.tambunan@shopee.com>
2022-08-18 15:34:11 +0700

    refactor: change query in FindPostBySlug

--- dto/get_post_res.go
+++ dto/get_post_res.go
@@ -3,17 +3,21 @@ package dto
 import "final-project-backend/models"
 
 type GetPostRes struct {
-	ID           int                  `json:"postID" gorm:"primaryKey"`
-	PostTier     *models.PostTier     `json:"postTier"`
-	PostCategory *models.PostCategory `json:"postCategory"`
-	Title        string               `json:"title"`
-	Content      string               `json:"content"`
-	Slug         string               `json:"slug"`
-	Summary      string               `json:"summary"`
-	ImgThumbnail *models.Image        `json:"imgThumbnail"`
-	ImgContent   *models.Image        `json:"imgContent"`
-	CreatedBy    *models.User         `json:"createdBy"`
-	UpdatedBy    *models.User         `json:"updatedBy"`
+	ID             int                  `json:"postID" gorm:"primaryKey"`
+	PostTier       *models.PostTier     `json:"postTier"`
+	PostCategory   *models.PostCategory `json:"postCategory"`
+	Title          string               `json:"title"`
+	Content        string               `json:"content"`
+	Slug           string               `json:"slug"`
+	Summary        string               `json:"summary"`
+	ImgThumbnailID int                  `json:"imgThumbnailID"`
+	ImgContentID   int                  `json:"imgContentID"`
+	CreatedByID    int                  `json:"createdByID"`
+	UpdatedByID    int                  `json:"updatedByID"`
+	//ImgThumbnail *models.Image        `json:"imgThumbnail"`
+	//ImgContent   *models.Image        `json:"imgContent"`
+	//CreatedBy    *models.User         `json:"createdBy"`
+	//UpdatedBy    *models.User         `json:"updatedBy"`
 }
 
 func (_ GetPostRes) FromPost(p *models.Post) *GetPostRes {
@@ -25,9 +29,13 @@ func (_ GetPostRes) FromPost(p *models.Post) *GetPostRes {
 		Content:      p.Content,
 		Slug:         p.Slug,
 		Summary:      p.Summary,
-		ImgThumbnail: p.ImgThumbnail,
-		ImgContent:   p.ImgContent,
-		CreatedBy:    p.CreatedBy,
-		UpdatedBy:    p.UpdatedBy,
+		//ImgThumbnail: p.ImgThumbnail,
+		//ImgContent:   p.ImgContent,
+		//CreatedBy:    p.CreatedBy,
+		//UpdatedBy:    p.UpdatedBy,
+		ImgThumbnailID: p.ImgThumbnailID,
+		ImgContentID:   p.ImgContentID,
+		CreatedByID:    p.CreatedByID,
+		UpdatedByID:    p.UpdatedById,
 	}
 }
--- repositories/post_repositories.go
+++ repositories/post_repositories.go
@@ -1,5 +1,6 @@
 package repositories
 
+import "C"
 import (
 	"final-project-backend/models"
 	"gorm.io/gorm"
@@ -29,6 +30,32 @@ func (repo *postRepository) FindPost(post *models.Post) (*models.Post, error) {
 }
 
 func (repo *postRepository) FindPostBySlug(slug string) (*models.Post, error) {
+	// TEST VIA RAW
+	//query := "SELECT posts.id,posts.post_tier_id,posts.post_category_id,posts.title,posts.content,posts.slug,posts.summary,posts.img_thumbnail_id,posts.img_content_id,posts.created_by_id,posts.updated_by_id,"
+	//query += " C.id AS PostCategory__id,C.created_at AS PostCategory__created_at,C.updated_at AS PostCategory__updated_at,C.deleted_at AS PostCategory__deleted_at,C.name AS PostCategory__name"
+	//query += " FROM posts LEFT JOIN post_categories C ON posts.post_category_id = C.id AND C.deleted_at IS NULL"
+	//
+	//post := &models.Post{}
+	//result := repo.db.
+	//	Raw(query).
+	//	Scan(&post)
+	//return post, result.Error
+
+	// GET SQL QUERY
+	//post := &models.Post{}
+	//sql := repo.db.
+	//	ToSQL(func(tx *gorm.DB) *gorm.DB {
+	//		return tx.
+	//			//Joins("PostTier").
+	//			Joins("PostCategory").
+	//			Where("slug = ?", slug).
+	//			First(&post)
+	//
+	//	})
+	//fmt.Println("================ THE SQL ==================")
+	//fmt.Println(sql)
+	//return nil, nil
+
 	post := &models.Post{}
 	result := repo.db.
 		Joins("PostTier").
@@ -39,4 +66,5 @@ func (repo *postRepository) FindPostBySlug(slug string) (*models.Post, error) {
 		Where("slug = ?", slug).
 		First(&post)
 	return post, result.Error
+
 }
